ffcd9bf817b618865a88dacef70d928d63af30d1
R21Ranjith <144472792+R21Ranjith@users.noreply.github.com>
2025-09-19 20:40:02 +0530

    Add files via upload

---
+++ mainf.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Variables
+  String name = "Flutter";
+  int version = 3;
+  double rating = 4.9;
+  bool isStable = true;
+
+  // Print
+  print("Hello, $name!");
+  print("Version: $version, Rating: $rating, Stable: $isStable");
+
+  // Arithmetic
+  int a = 10, b = 20;
+  print("Sum = ${a + b}");
+
+  // List
+  List<String> features = ["Hot Reload", "Cross-platform", "Widgets"];
+  print("Features: $features");
+
+  // Map
+  Map<String, dynamic> info = {"language": "Dart", "platform": "Mobile"};
+  print(info);
+}
